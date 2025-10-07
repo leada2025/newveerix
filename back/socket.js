4648ef6be27415721b62fca172967a50aa926b99
unknown <webdevldd@gmail.com>
2025-10-07 18:48:09 +0530

    test

--- back/socket.js
+++ back/socket.js
@@ -7,7 +7,7 @@ module.exports = {
   init: (server) => {
     io = require("socket.io")(server, {
       cors: {
-       origin: ["http://localhost:5173","http://localhost:5174", "https://newveerix.vercel.app"],
+       origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175", "https://newveerix.vercel.app"],
         methods: ["GET", "POST", "PATCH"],
         credentials: true,
       },
