af1604fa7a376e4cfc9248c93953e10c11bf03f2
unknown <webdevldd@gmail.com>
2025-10-07 19:29:34 +0530

    cors again

--- back/server.js
+++ back/server.js
@@ -32,7 +32,7 @@ const corsOptions = {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true,
+  
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
@@ -60,4 +60,6 @@ const server = http.createServer(app);
 const io = socket.init(server);
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
