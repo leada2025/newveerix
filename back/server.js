6460b082b81c8f69bf9c5227c90ae2757e8dedfc
unknown <webdevldd@gmail.com>
2025-10-08 17:30:38 +0530

    same

--- back/server.js
+++ back/server.js
@@ -17,6 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: ["http://localhost:5173","http://localhost:5174", "https://newveerix.vercel.app"],
+   methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
   credentials: true,
 }));
 
